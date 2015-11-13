f44972fa6367514842b24dd9e6c5f02a38d5fd05
oosawak <oosawak@Oosawak-no-MacBook-Air.local>
2015-11-13 12:18:57 +0900

    2015/11/13 12:18
    Unity2dでクリックすると爆発するを沢山配置するサンプル
    Unity2dでのSprite、Rigidbody2D、パーティクルの実装、Purefabの動的配置のサンプル
    
    ・爆弾をクリックすると爆発してパーティクルを撒き散らします。
    ・爆弾は慣性で移動します。

---
+++ Assets/Scripts/bg_back.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bg_back : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		int enemycnt = 0 ;
+		int enemymax = 100 ;
+		if(enemycnt < enemymax) {
+			for (; enemycnt < enemymax; enemycnt++) {
+				Enemy.Add (Random.Range (100, 300), Random.Range (100, 300));
+			}
+			enemycnt--;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
